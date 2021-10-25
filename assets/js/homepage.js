6e9fe71efbdf4c7845636a917bb812679699ec92
trm621 <t.maher621@gmail.com>
2021-10-25 15:07:17 -0400

    added user serverside api interactivity

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+    });
+    });
 };
 
 getUserRepos();
